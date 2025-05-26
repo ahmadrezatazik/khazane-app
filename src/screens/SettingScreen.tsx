97679f00780416f03764764b329d9d0e19958dda
ahmadrezatazik <99529856+ahmadrezatazik@users.noreply.github.com>
2025-05-26 08:59:37 +0330

    Create SettingScreen.tsx

---
+++ src/screens/SettingScreen.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+
+export default function SettingScreen({ navigation }) {
+  useEffect(() => {
+    setTimeout(() => navigation.replace('Home'), 1500);
+  }, []);
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>💰 خزانه</Text>
+      <ActivityIndicator size="large" />
+    </View>
+  );
+}
